68581c5b64646163877fac07007e2ad8374e7a1e
AlirezaMashayekhy <alireza@sheydal.com>
2026-07-13 16:20:35 +0330

    fix error

--- src/b2b-request/dto/update-b2b-request.dto.ts
+++ src/b2b-request/dto/update-b2b-request.dto.ts
@@ -1,4 +1,5 @@
 import { PartialType } from '@nestjs/swagger';
-import { CreateB2bRequestDto } from './create-b2b-request.dto';
 
-export class UpdateB2bRequestDto extends PartialType(CreateB2bRequestDto) {}
+import { CreateB2BRequestDto } from './create-b2b-request.dto';
+
+export class UpdateB2bRequestDto extends PartialType(CreateB2BRequestDto) {}
